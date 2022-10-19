77a1770e54ac07618f33283562ba151188b378c9
Mauricio Acosta <bernardoz209@icloud.com>
2022-10-19 13:26:55 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Mauricio Acosta
+//10-19-22
+//GitHub Reverse It
+
+string playAgain = "yes";
+while (playAgain == "yes")
+{
+Console.WriteLine("Choose several numbers");
+
+string userInput = Console.ReadLine();
+
+
+for(int i = userInput.Length - 1; i >= 0; i--)
+{
+Console.WriteLine(userInput[i]);
+}
+
+Console.WriteLine("Type yes to play again");
+playAgain = Console.ReadLine();
+
+}
